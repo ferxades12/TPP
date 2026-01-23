a57eed43deeb610ef3151ad183d703a1be4facb1
ferxades12 <ferxades12@gmail.com>
2026-01-23 16:44:52 +0100

    Sesion 1.1

---
+++ TPP01_2526/ClassLibrary1/Class1.cs
@@ -0,0 +1,87 @@
+﻿namespace ClassLibrary1;
+
+
+public enum Status {VIP, Business, Standard};
+
+public class Cliente
+{
+    public string Id { get; set; } = "";
+    public string _nombre = "";
+    public string Nombre
+    {
+        get { return _nombre; }
+        set
+        {
+            if (value == null)
+                throw new ArgumentException("El nombre no puede ser null");
+            _nombre = value;
+        }
+    }
+    public string Apellido { get; set; } = "";
+    public int _edad = 0;
+    public int Edad
+    {
+        get { return _edad; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("La edad no puede ser negativa");
+            _edad = value;
+        }
+    }
+    public Status Status { get; set; } = Status.Standard;
+    /* public Cliente(string nombre)
+    {
+        Nombre = nombre;
+    } */
+
+    override public string ToString()
+    {
+        return $"Nombre: {Nombre}, Apellido: {Apellido}, Edad: {Edad}, Status: {Status}";
+    }
+}
+
+public enum OrderStatus {Borrador, Pagado, Enviado, Entregado, Cancelado};
+class Order
+{
+    public string Identificador { get; set; } = "";
+    public string Nombre { get; set; } = "";
+    public decimal Cantidad { get; set; } = 0.0M;
+    public decimal Precio { get; set; } = 0.0M;
+    public decimal GastosEnvio { get; set; } = 0.0M;
+    public decimal Subtotal
+    {
+        get { return Cantidad * Precio; }
+    }
+    public decimal Total { 
+        get { return Subtotal + GastosEnvio; } 
+    }
+    public OrderStatus Status { get; set; } = OrderStatus.Borrador; 
+}
+
+public enum MealPlan {Alojamiento, Desayuno, MediaPension, PensionCompleta};
+
+public class HotelStay
+{
+    public string Id = "";
+    public string Hotel = "";
+    public DateTime FechaEntrada;
+    public DateTime FechaSalida;
+    public decimal TarifaPorNoche = 0.0M;
+    public decimal NumeroNoches
+    {
+        get
+        {
+            int days = (FechaSalida - FechaEntrada).Days;
+            if (days < 0)
+                throw new ArgumentException("La fecha de salida no puede ser anterior a la de entrada");
+            return days;
+        }
+    }
+    public decimal Total
+    {
+        get { return NumeroNoches * TarifaPorNoche; }
+    }
+
+    public MealPlan RegimenComidas { get; set; } = MealPlan.Alojamiento;
+}
---
+++ TPP01_2526/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1;
+using ClassLibrary1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Cliente c1 = new Cliente("Alice");
+        //Console.WriteLine($"Hola, {c1.Nombre}!");
+        Cliente c2 = new Cliente{Nombre = "Alicia", Edad = 30};
+        Console.WriteLine(c2);
+
+        HotelStay hs1 = new HotelStay
+        {
+            Id = "1",
+            Hotel = "Hotel",
+            FechaEntrada = DateTime.Now.AddDays(5),
+            FechaSalida = DateTime.Now,
+            TarifaPorNoche = 120.0M,
+            RegimenComidas = MealPlan.MediaPension
+        };
+    }
+}
