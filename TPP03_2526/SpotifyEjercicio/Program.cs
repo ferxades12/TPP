b3f117976949d5be3f05aaadd5eac106d1551a10
ferxades12 <ferxades12@gmail.com>
2026-02-06 15:22:55 +0100

    add P03 files

---
+++ TPP03_2526/SpotifyEjercicio/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SpotifyEjercicio;
+
+/// <summary>
+/// Replantea código del proyecto Spotify empleando ISP.
+/// </summary>
+class Program
+{
+    static void Main()
+    {
+/*         
+        var spotify = new Spotify();
+        var song1 = new Song("Canción 1");
+        var song2 = new Song("Canción 2");
+        var podcast1 = new Podcast("Podcast 1");
+        var radio1 = new Radio("Radio 1");
+ */
+    // Combinaciones de las anteriores en distintos arrays con distintos tipos...
+
+    }
+
+    interface IDownloadable { void Download(); }
+}
