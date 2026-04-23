108e22733685f179e02be2d88403c835c0cae28b
ferxades12 <ferxades12@gmail.com>
2026-04-23 15:43:17 +0200

    Entrgas11

---
+++ Entregas/11-ConcurrentQueue/ConcurentQueue/Class1.cs
@@ -0,0 +1,59 @@
+﻿namespace ConcurrentQueue;
+
+public class ConcurrentQueue<T>
+{
+    private GenericLinkedList<T> list;
+    private readonly object lockObject = new object();
+    public int Count => _count;
+    public int _count;
+
+    public ConcurrentQueue()
+    {
+        list = new GenericLinkedList<T>();
+    }
+
+    public bool isEmpty()
+    {
+        return Interlocked.CompareExchange(ref _count, 0, 0) == 0;
+    }
+
+    public void Enqueue(T? value)
+    {
+        lock (lockObject)
+        {
+            list.Add(value);
+
+            _count++;
+        }
+    }
+
+    public T? Dequeue()
+    {
+        lock (lockObject)
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            T? value = list.ElementAt(0);
+            list.RemoveAt(0);
+
+            _count--;
+            return value;
+        }
+    }
+
+    public T? Peek()
+    {
+        lock (lockObject)
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return list.ElementAt(0);
+        }
+    }
+}
